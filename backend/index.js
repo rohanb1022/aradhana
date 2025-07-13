3cb9a193baff0775818b51effb55f920b05ccb49
Rohan Bhangale <rohanbhangale25@gmail.com>
2025-07-13 10:03:38 +0530

    fix: issue related to render accessing and biding index.js to default DNS

--- backend/index.js
+++ backend/index.js
@@ -61,9 +61,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal server error" });
 });
 
+const PORT = process.env.PORT || 10000;
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
